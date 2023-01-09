5306b0172c8d2117d9b5764f645f6bc4ba432824
Luke Swanson <lwswanson2@wisc.edu>
2023-01-09 14:31:43 -0600

    Remove references to About and Datasets from App.

--- view/App.js
+++ view/App.js
@@ -3,8 +3,6 @@ import './App.scss';
 import { React } from 'react'
 // import './assets/scss/styles_ver1.scss';
 import Navigation from '../components/navigation/Navigation';
-import About from './pages/About/About';
-import Datasets from './pages/Datasets/Datasets';
 import Dashboard from './pages/Dashboard/Dashboard';
 import NotFound from "./pages/NotFound";
 
@@ -16,8 +14,6 @@ function App() {
       <div className='App h-screen w-screen pt-12 bg-gradient-to-br from-white via-stone-50 to-stone-200'>
         <Routes>
           <Route path={base_path+"/"} element={<Dashboard />} />
-          <Route path={base_path+"about"} element={<About />} />
-          <Route path={base_path+"datasets"} element={<Datasets />} />
           {/* <Route path={base_path+"dashboard"} element={<Dashboard />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
