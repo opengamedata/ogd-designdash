fdb9e6c0c838c7fb814c57b924f8791c8f79d98b
Luke Swanson <lwswanson2@wisc.edu>
2023-01-12 11:56:29 -0600

    Move base directory to config.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
 // import './assets/scss/styles_ver1.scss';
+import { DASHBOARD_BASE } from './config';
 import Navigation from './components/navigation/Navigation';
 import Dashboard from './view/Dashboard';
 import NotFound from "./view/NotFound";
 
 function App() {
-  let base_path = "/opengamedata-testing2/"; // TODO: this goes in a config or something
   return (
     <>
       <Navigation />
       <div className='App h-screen w-screen pt-12 bg-gradient-to-br from-white via-stone-50 to-stone-200'>
         <Routes>
-          <Route path={base_path+"/"} element={<Dashboard />} />
+          <Route path={DASHBOARD_BASE+"/"} element={<Dashboard />} />
           {/* <Route path={base_path+"dashboard"} element={<Dashboard />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
