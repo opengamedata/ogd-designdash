49db8ea439fd787831abbfd61be19ca3beb847d6
Luke Swanson <lwswanson2@wisc.edu>
2023-01-12 09:32:45 -0600

    Add a file to hold all the type hinting definitions.

---
+++ src/typedefs.js
@@ -0,0 +1,56 @@
+import { JobGraphModel } from "./model/visualizations/JobGraphModel";
+import { ViewModes } from "./model/ViewModes";
+
+/**
+ * @typedef FeaturesMap
+ * @property {Object.<string, string[]>} map
+ */
+
+/**
+ * @callback SetterCallback
+ * @param {object} newVal
+ */
+
+/**
+ * @callback JobGraphSetter
+ * @param {JobGraphModel} newVal
+ */
+
+/**
+ * @callback ViewModeSetter
+ * @param {ViewModes} newVal
+ */
+
+/**
+ * @callback StringSetter
+ * @param {string} newVal
+ */
+
+/**
+ * @callback StringListSetter
+ * @param {string[]} newVal
+ */
+
+/**
+ * @callback PropertyCallback
+ */
+
+/**
+ * @callback ElementSetter
+ * @param {ElementRenderer} newRenderer
+ */
+
+/**
+ * @callback ElementRenderer
+ * @returns {Element} newVal
+ */
+
+/**
+ * @callback PropertySetter
+ * @param {PropertyCallback} newCallback
+ */
+
+/**
+ * @callback FeatureMapSetter
+ * @param {FeaturesMap} newVal
+ */
