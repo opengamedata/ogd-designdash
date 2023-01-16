cdfcb56ba37e2b65a25820a73608d6854ac770c5
LswaN58 <lwswanson2@wisc.edu>
2023-01-16 15:17:40 -0600

    Step 1 of migrating to class-based components, move App.js over.
    Had been kinda avoiding this because (a) existing code was all as
    functions, and (b) I'm not totally sure what the implications are for
    browswer compatibility.
    However, seems I can do it one piece at a time, so (a) isn't so bad.
    As to (b), we'll cross that bridge if we ever come to it.
    Until then, makes following guidelines from React's documentation a bit
    easier.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import Navigation from './components/navigation/Navigation';
 import Dashboard from './view/Dashboard';
 import NotFound from "./view/NotFound";
 
-function App() {
-  return (
-    <>
-      <Navigation />
-      <div className='App h-screen w-screen pt-12 bg-gradient-to-br from-white via-stone-50 to-stone-200'>
-        <Routes>
-          <Route path={DASHBOARD_BASE+"/"} element={<Dashboard />} />
-          {/* <Route path={base_path+"dashboard"} element={<Dashboard />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <Navigation />
+        <div className='App h-screen w-screen pt-12 bg-gradient-to-br from-white via-stone-50 to-stone-200'>
+          <Routes>
+            <Route path={DASHBOARD_BASE+"/"} element={<Dashboard />} />
+            {/* <Route path={base_path+"dashboard"} element={<Dashboard />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
