0b598fb53eeebd85624335a4b4e18d9691a4cc6b
LswaN58 <lwswanson2@wisc.edu>
2023-01-16 15:28:11 -0600

    Make ErrorBoundary error message more useful.

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -16,7 +16,7 @@ export class ErrorBoundary extends React.Component {
 
    render() {
       if (this.state.hasError) {      // You can render any custom fallback UI
-         return <h1>Something went wrong.</h1>;
+         return <h1>Error in child component of type {this.props.childType}</h1>;
       }
       return this.props.children;
    }
