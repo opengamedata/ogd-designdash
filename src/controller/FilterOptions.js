75715927eafff53748ca1dcbde87cedabfef6a79
LswaN58 <lwswanson2@wisc.edu>
2023-01-16 14:19:59 -0600

    Ok, actually, let's make sure the playtimes aren't null, because we want to call functions on them later.

--- src/controller/FilterOptions.js
+++ src/controller/FilterOptions.js
@@ -3,10 +3,10 @@ import Timedelta from "../model/Timedelta";
 export class FilterOptions {
    /**
     * @param {number} min_jobs
-    * @param {Timedelta | null} min_playtime
-    * @param {Timedelta | null} max_playtime
+    * @param {Timedelta} min_playtime
+    * @param {Timedelta} max_playtime
     */
-   constructor(min_jobs=1, min_playtime=null, max_playtime=null) {
+   constructor(min_jobs=1, min_playtime=new Timedelta(), max_playtime=new Timedelta()) {
       this.min_jobs = min_jobs;
       this.min_playtime = min_playtime;
       this.max_playtime = max_playtime;
