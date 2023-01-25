2fdd5dfcbed4718ac472c0e48830659ce14dbcaa
LswaN58 <lwswanson2@wisc.edu>
2023-01-25 09:37:29 -0600

    Add a separate component for picking a game from list.

---
+++ src/view/DataFilter/GamePicker.js
@@ -0,0 +1,63 @@
+// global imports
+import React from 'react';
+// local imports
+import { vis_games } from '../../config';
+import { ViewModes } from "../../model/ViewModes"
+
+/**
+ * @typedef {import("../../typedefs").SetterMap} SetterMap
+ * @typedef {import("../../typedefs").SetterCallback} SetterCallback
+ */
+
+/**
+ * @typedef  {object} GamePickerProps
+ * @property {boolean} adjustMode
+ * @property {string} gameSelected
+ * @property {SetterCallback} setGameSelected
+ */
+
+ /**
+ * @param {GamePickerProps} props
+ */
+export default function GamePicker({
+   adjustMode,
+   gameSelected,
+   setGameSelected
+}) {
+    const gameList = () => {
+        const games = []
+        vis_games.forEach((k) => {
+            games.push(
+                <option key={k} value={k}>{k}</option>
+            )
+        })
+        return games
+    }
+
+   if (adjustMode) {
+      return(
+         <div id="GameSelector" className="col">
+            <div className="input-group">
+                  <div className="mb-2">
+                     <span className='text-xl font-light'>Game</span>
+                  </div>
+                  <select
+                     className="form-select block w-full"
+                     value={gameSelected}
+                     onChange={(e) => setGameSelected(e.target.value)}>
+                     <option> </option>
+                     {gameList()}
+                  </select>
+            </div>
+         </div>
+      )
+   }
+   else {
+      return(
+         <div>
+            <span className='font-medium '>{gameSelected}&nbsp;</span>
+         </div>
+      )
+      // return <div>SelectionChoices</div>
+   }
+}
