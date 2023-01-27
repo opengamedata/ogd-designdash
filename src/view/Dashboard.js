c0a69e72d44ee4d0892c0f6210d93ce17cdf6a0a
LswaN58 <lwswanson2@wisc.edu>
2023-01-26 18:46:50 -0600

    Styling/layout improvements at Dashboard level.

--- src/view/Dashboard.js
+++ src/view/Dashboard.js
@@ -7,7 +7,7 @@ import VizContainer from './VizContainer'
 export default function Dashboard() {
 
    return (
-        <div className='container relative flex flex-wrap flex-col'>
+        <div className='container flex flex-col'>
             {/* For DEBUG purpose, remove in production */}
             <div className='fixed top-0 right-1/2 z-10'>
                 <LargeButton
@@ -16,15 +16,15 @@ export default function Dashboard() {
                     onClick={() => { localStorage.clear(); alert('localStorage reset') }}
                 />
             </div>
-            <div id="Description" className='max-w-xl m-4'>
+            <div id="Description" className='h-fit max-w-xl m-4'>
                 <p className='mb-3 text-4xl font-light'>Designer Dashboard</p>
                 <p>
                     A visualization tool for you to intuitively interpret data collected from gameplays.
                     Pick a game and a time range to begin.
                 </p>
             </div>
-            <div id="Container" className='relative border border-red-700'>
-                <VizContainer></VizContainer>
+            <div id="ContainerCollection" className='relative grid grid-flow-row-dense'>
+                <VizContainer column={1} row={1}></VizContainer>
             </div>
             {/* In the future, add controls to manage multiple visualizations, which each get own filtering options. */}
         </div>
