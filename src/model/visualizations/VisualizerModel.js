690f78198635a5f0ea721f4950c335b9307588fe
LswaN58 <lwswanson2@wisc.edu>
2023-01-27 01:20:23 -0600

    Forgot to save game name, and add props to models.

--- src/model/visualizations/VisualizerModel.js
+++ src/model/visualizations/VisualizerModel.js
@@ -5,6 +5,14 @@ export default class VisualizerModel {
     * @param {object} raw_data 
     */
    constructor(game_name, raw_data) {
+      this.game_name = game_name
       this.raw_data = raw_data
+   }
+
+   get Game() {
+      return this.game_name;
+   }
+   get Data() {
+      return this.raw_data
    }
 }
