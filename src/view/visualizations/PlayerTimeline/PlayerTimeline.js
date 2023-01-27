7f98dba3727db5a7ed350fb4792bd162d06323cc
LswaN58 <lwswanson2@wisc.edu>
2023-01-27 01:26:23 -0600

    And rename here too.

--- src/view/visualizations/PlayerTimeline/PlayerTimeline.js
+++ src/view/visualizations/PlayerTimeline/PlayerTimeline.js
@@ -10,7 +10,7 @@ import EventFilterCtrl from './EventFilterCtrl';
 import timeline from "./timeline";
 
 
-export default function PlayerVisualizer({ rawData, setViewMode, selectedGame }) {
+export default function PlayerTimeline({ rawData, setViewMode, selectedGame }) {
 
     const convertedData = convert(rawData)
 
