4020ff2696bee922a62596a196f650be2340839f
LswaN58 <lwswanson2@wisc.edu>
2023-01-27 00:35:01 -0600

    Add placeholder request classes for JobGraph and PlayerTimeline.

---
+++ src/model/requests/JobGraphRequest.js
@@ -0,0 +1,36 @@
+import VisualizerRequest from "./VisualizerRequest";
+import { AvailableGames } from "../enums/AvailableGames";
+import { FilterRequest, FilterItem, InputModes, ValueModes } from "./FilterRequest";
+
+/**
+ * @typedef {import("../../typedefs").MapSetter} MapSetter
+ * @typedef {import("./APIRequest").APIRequest} APIRequest
+ */
+
+export default class JobGraphRequest extends VisualizerRequest {
+   /**
+    * @param {MapSetter} updateRequesterState
+    */
+   constructor(updateRequesterState) {
+      super(updateRequesterState);
+      this.filter_request = new FilterRequest(this.updateRequesterState);
+      this.filter_request.AddItem(
+         new FilterItem("Game", InputModes.DROPDOWN, ValueModes.ENUM, {"enum":AvailableGames})
+      )
+   }
+
+   /**
+    * @param {object} requesterState
+    * @returns {APIRequest?} The API request that gets the visualizer's required data.
+    */
+   GetAPIRequest(requesterState) {
+      return null;
+   }
+
+   /**
+    * @returns {FilterRequest}
+    */
+   GetFilterRequest() {
+      return this.filter_request;
+   }
+}
