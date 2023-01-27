88f1f7953c33584ea501b35d34e3b6568728314d
LswaN58 <lwswanson2@wisc.edu>
2023-01-26 21:52:14 -0600

    API itself also needs to accept null request.
    Just does same as before for default case, returning "no request"

--- src/controller/apis/OGDAPI.js
+++ src/controller/apis/OGDAPI.js
@@ -8,30 +8,37 @@ import RequestModes from '../../model/enums/RequestModes';
  */
 
 /**
- * @param {APIRequest} request
+ * @param {APIRequest?} request
  * @returns {Promise<Response>}
  */
 export class OGDAPI {
    static fetch(request) {
-      switch (request.RequestMode) {
-         case RequestModes.POPULATION:
-            return OGDPopulationAPI.fetch(request);
-         break;
-         case RequestModes.PLAYER:
-            return OGDPlayerAPI.fetch(request);
-         break;
-         case RequestModes.SESSION:
-            return OGDSessionAPI.fetch(request);
-         break;
-         default:
-            let response_data = {
-               result: "No Data Requested",
-               status: "SUCCESS",
-               val: "{}"
-            }
-            let dummy = new Response(JSON.stringify(response_data), {status:200});
-            return Promise.resolve(dummy);
-         break;
+      const no_request = {
+         result: "No Data Requested",
+         status: "SUCCESS",
+         val: "{}"
+      };
+
+      if (request != null) {
+         switch (request.RequestMode) {
+            case RequestModes.POPULATION:
+               return OGDPopulationAPI.fetch(request);
+            break;
+            case RequestModes.PLAYER:
+               return OGDPlayerAPI.fetch(request);
+            break;
+            case RequestModes.SESSION:
+               return OGDSessionAPI.fetch(request);
+            break;
+            default:
+               let dummy = new Response(JSON.stringify(no_request), {status:200});
+               return Promise.resolve(dummy);
+            break;
+         }
+      }
+      else {
+         let dummy = new Response(JSON.stringify(no_request), {status:200});
+         return Promise.resolve(dummy);
       }
    }
 }
