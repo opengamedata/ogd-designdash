ce83adc3bd58259f10798fe789c3aad30446a4c3
LswaN58 <lwswanson2@wisc.edu>
2023-01-27 09:07:16 -0600

    Make EnumPicker gracefully handle null enumType.

--- src/components/EnumPicker.js
+++ src/components/EnumPicker.js
@@ -46,49 +46,57 @@ export default function EnumPicker({
                          : "Empty";
    /** @type {[EnumType, any]} */
    const [localSelection, setLocalSelection] = useState(defaultSelection)
-   console.log(`To start off, EnumPicker for filterItem ${filterItem.Name} has localSelection of ${localSelection}`)
 
-   console.log(`In EnumPicker, the EnumList is: ${enumType.EnumList()}`)
-   const optionList = enumType.EnumList().map((k) => {
-      let next_key = `${filterItem.Name}${k.asString}`;
-      return (<option key={next_key} value={k.asString}>{k.asDisplayString}</option>)
-   })
+   if (enumType != undefined) {
+      console.log(`To start off, EnumPicker for filterItem ${filterItem.Name} has localSelection of ${localSelection}`)
+      console.log(`In EnumPicker, the EnumList is: ${enumType != undefined ? enumType.EnumList() : 'enumType is ' + enumType.toString()}`)
 
-   const updateSelection = (e) => {
-      setLocalSelection(enumType.FromName(e.target.value));
-      try {
-         if (filterItem.Validator({'selected':localSelection})) {
-            updateFilterState(`${filterItem.Name}Selected`, localSelection);
+      const optionList = enumType.EnumList().map((k) => {
+         let next_key = `${filterItem.Name}${k.asString}`;
+         return (<option key={next_key} value={k.asString}>{k.asDisplayString}</option>)
+      })
+
+      const updateSelection = (e) => {
+         setLocalSelection(enumType.FromName(e.target.value));
+         try {
+            if (filterItem.Validator({'selected':localSelection})) {
+               updateFilterState(`${filterItem.Name}Selected`, localSelection);
+            }
+         }
+         catch (error) {
+            alert(error);
+            return;
          }
       }
-      catch (error) {
-         alert(error);
-         return;
-      }
-   }
 
-   if (adjustMode) {
-      return(
-         <div id={`${localSelection.constructor.name}Selector`} className="col">
-            <div className="input-group">
-                  <div className='text-base font-semibold mb-2'>Game</div>
-                  <select
-                     className="form-select block w-full"
-                     value={`${filterItem.Name}${localSelection.asString}`}
-                     onChange={updateSelection}>
-                     <option key="Empty"> </option>
-                     {optionList}
-                  </select>
+      if (adjustMode) {
+         return(
+            <div id={`${localSelection.constructor.name}Selector`} className="col">
+               <div className="input-group">
+                     <div className='text-base font-semibold mb-2'>Game</div>
+                     <select
+                        className="form-select block w-full"
+                        value={`${filterItem.Name}${localSelection.asString}`}
+                        onChange={updateSelection}>
+                        <option key="Empty"> </option>
+                        {optionList}
+                     </select>
+               </div>
             </div>
-         </div>
-      )
+         )
+      }
+      else {
+         return(
+            <div>
+               <span className='font-medium '>{localSelection.asDisplayString}&nbsp;</span>
+            </div>
+         )
+         // return <div>SelectionChoices</div>
+      }
    }
    else {
       return(
-         <div>
-            <span className='font-medium '>{localSelection.asDisplayString}&nbsp;</span>
-         </div>
+         <div>Invalid EnumPicker, <code>enumType</code> is <code>{enumType}</code>!</div>
       )
-      // return <div>SelectionChoices</div>
    }
 }
