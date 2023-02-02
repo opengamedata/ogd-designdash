6d787b2662060b6bfb67280057b5cc62e2e53733
LswaN58 <lwswanson2@wisc.edu>
2023-02-02 10:04:31 -0600

    Fix quotations and remove debug logging.

--- src/components/LoadingBlur.js
+++ src/components/LoadingBlur.js
@@ -3,9 +3,10 @@ import { Cog6ToothIcon } from '@heroicons/react/24/solid'
 
 export default function LoadingBlur({ loading, height, width }) {
 
+    console.log(`The LoadingBlur height and width should be ${height} and ${width}`)
     return loading ?
         <div className="absolute left-0 top-0 w-full h-full z-10 backdrop-blur-md flex justify-center items-center space-x-3">
-            <Cog6ToothIcon className={`'animate-spin h-${height} w-${width}'`} />
+            <Cog6ToothIcon className={`animate-spin h-${height} w-${width}`} />
             <p className="text-3xl font-light">Loading...</p>
         </div>
         :
