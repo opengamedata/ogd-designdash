1b230c821e08f7f10905c01a1a9a156919d78b13
LswaN58 <lwswanson2@wisc.edu>
2023-02-02 09:42:51 -0600

    Make loading blur sized to the container.

--- src/components/LoadingBlur.js
+++ src/components/LoadingBlur.js
@@ -4,7 +4,7 @@ import { Cog6ToothIcon } from '@heroicons/react/24/solid'
 export default function LoadingBlur({ loading, height, width }) {
 
     return loading ?
-        <div className="absolute left-0 top-0 w-screen h-screen z-10 backdrop-blur-md flex justify-center items-center space-x-3">
+        <div className="absolute left-0 top-0 w-full h-full z-10 backdrop-blur-md flex justify-center items-center space-x-3">
             <Cog6ToothIcon className={`'animate-spin h-${height} w-${width}'`} />
             <p className="text-3xl font-light">Loading...</p>
         </div>
