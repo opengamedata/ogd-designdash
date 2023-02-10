d55d3881501d0bbf19b4003009367f2a48aa66ed
LswaN58 <lwswanson2@wisc.edu>
2023-02-10 10:06:30 -0600

    Refactor buttons a bit for easier readability/styling

--- src/components/buttons/LargeButton.js
+++ src/components/buttons/LargeButton.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export default function LargeButton({ selected, onClick, label }) {
-    const baseStyle = "px-7 py-2 my-3 border border-slate-800 transition-colors ease-in-out duration-300 font-medium text-xl hover:text-yellow-300 hover:bg-slate-800 rounded-md text-sm "
+    const spacing      = "px-7 py-2 my-3";
+    const border       = "border border-slate-800 rounded-md";
+    const font         = "font-medium text-xl text-sm";
+    const color        = selected ? 'text-yellow-300 bg-slate-800' : 'bg-white';
+    const animation    = "transition-colors ease-in-out duration-300";
+    const interactions = "hover:text-yellow-300 hover:bg-slate-800";
+    const styling = `${spacing} ${border} ${font} ${color} ${interactions} ${animation}`
 
     return (
-        <button
-            className={selected ? baseStyle + 'text-yellow-300 bg-slate-800' : baseStyle + 'bg-white'}
-            onClick={onClick}
-        >
+        <button className={styling} onClick={onClick}>
             {label}
         </button>
     )
-
 }
--- src/components/buttons/LongButton.js
+++ src/components/buttons/LongButton.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-export default function LongButton({ selected, action, label }) {
-    const baseStyle = "w-full px-2 py-1 mr-2 my-1 shadow-sm border border-stone-300 transition-colors ease-in-out duration-300 font-medium hover:text-yellow-300 hover:bg-slate-800 rounded-sm text-sm "
+export default function LongButton({ selected, onClick, label }) {
+    const spacing      = "w-full px-2 py-1 mr-2 my-1";
+    const border       = "border border-stone-300 rounded-sm shadow-sm";
+    const font         = "font-medium text-sm";
+    const color        = selected ? 'text-yellow-300 bg-slate-800' : 'bg-white';
+    const animation    = "transition-colors ease-in-out duration-300";
+    const interactions = "hover:text-yellow-300 hover:bg-slate-800";
+    const styling = `${spacing} ${border} ${font} ${color} ${interactions} ${animation}`
 
     return (
-        <button
-            className={selected ? baseStyle + 'text-yellow-300 bg-slate-800' : baseStyle + 'bg-white'}
-            onClick={action}
-        >
+        <button className={styling} onClick={onClick}>
             {label}
         </button>
     )
-
 }
