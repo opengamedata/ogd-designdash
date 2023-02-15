b6a78dff6734a47ad8ca934f5a1e41e6b538845f
LswaN58 <lwswanson2@wisc.edu>
2023-02-14 23:38:08 -0600

    Add a function to use local time to create ISO-style format.

--- src/controller/TimeFormat.js
+++ src/controller/TimeFormat.js
@@ -18,6 +18,23 @@ export function ISODateFormat(date) {
 }
 
 /**
+ * @param {Date} date 
+ * @returns {string}
+ */
+export function ISODatetimeFormat(date) {
+   let second = leftPad(date.getSeconds(), 2, "0");
+   let minute = leftPad(date.getMinutes(), 2, "0");
+   let hour   = leftPad(date.getHours(), 2, "0");
+   let day    = leftPad(date.getDate(), 2, "0");
+   let month  = leftPad(date.getMonth()+1, 2, "0");
+   let year   = leftPad(date.getFullYear(), 4, "0");
+   const fmt_date = [year, month, day].join('-');
+   const fmt_time = [hour, minute, second].join(':')
+   const ret_val = [fmt_date, fmt_time].join('T')
+   return ret_val
+}
+
+/**
  * 
  * @param {Date} date 
  * @returns {string}
