de01348883b493fc6ddcdc2c332de6208b17cdf6
Luke Swanson <lwswanson2@wisc.edu>
2024-01-10 10:33:35 -0600

    Update RequestModes to allow more kinds of requests.

--- src/enums/RequestModes.js
+++ src/enums/RequestModes.js
@@ -4,9 +4,16 @@ export default class RequestModes extends EnumType {
    static POPULATION = new RequestModes('POPULATION', 'Population');
    static PLAYER = new RequestModes('PLAYER', 'Player');
    static SESSION = new RequestModes('SESSION', 'Session');
+   static POPULATION_AVAILABLE = new RequestModes('POPULATION_AVAILABLE', 'Population Available Metrics');
+   static PLAYER_AVAILABLE = new RequestModes('PLAYER_AVAILABLE', 'Player Available Metrics');
+   static SESSION_AVAILABLE = new RequestModes('SESSION_AVAILABLE', 'Session Available Metrics');
+   static PLAYER_LIST = new RequestModes('PLAYER_LIST', 'List of Players');
+   static SESSION_LIST = new RequestModes('SESSION_LIST', 'List of Sessions');
 
    static EnumList() {
-      return [RequestModes.POPULATION, RequestModes.PLAYER, RequestModes.SESSION]
+      return [RequestModes.POPULATION, RequestModes.PLAYER, RequestModes.SESSION,
+              RequestModes.POPULATION_AVAILABLE, RequestModes.PLAYER_AVAILABLE,
+              RequestModes.SESSION_AVAILABLE, RequestModes.PLAYER_LIST, RequestModes.SESSION_LIST]
    }
 
    static Default() {
