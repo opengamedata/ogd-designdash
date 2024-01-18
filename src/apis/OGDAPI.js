10beefbddfac98982440f1695e622f25bbf978b4
Luke Swanson <lwswanson2@wisc.edu>
2024-01-18 09:24:03 -0600

    Implement functions for the different kinds of HTTP requests.
    For now they're all the same, apart from the name of the method.
    In the future, might do something different to enforce where params are
    given in request classes?

--- src/apis/OGDAPI.js
+++ src/apis/OGDAPI.js
@@ -1,3 +1,4 @@
+import { API_ORIGIN } from '../config';
 import { RESTTypes } from '../enums/RESTTypes';
 
 /**
@@ -61,55 +62,71 @@ export class OGDAPI {
     * @returns {Promise<Response>}
     */
    static fetchGET(request) {
+         let searchParams = null;
+         if (Object.keys(request.HeaderParams()).length > 0) {
+            searchParams = []
+            let callback = (key) => searchParams.push(`${key}=${encodeURIComponent(request.HeaderParams()[key])}`);
+            Object.keys(request.HeaderParams()).forEach( callback );
+         }
 
+         // fetch by url
+         const _path = searchParams ? `${request.URLPath}?${searchParams.join("&")}` : request.URLPath()
+         const url = new URL(_path, API_ORIGIN)
+         console.log(`OGDPopulationAPI is making a request to ${url}`)
+         let options = {
+            method : "GET"
+         };
+         if (Object.keys(request.BodyParams()).length > 0) {
+            options.body = JSON.stringify(request.BodyParams())
+         }
+         return fetch(url, options);
    }
    /**
     * @param {APIRequest} request 
     * @returns {Promise<Response>}
     */
    static fetchPOST(request) {
+         let searchParams = null;
+         if (Object.keys(request.HeaderParams()).length > 0) {
+            searchParams = []
+            let callback = (key) => searchParams.push(`${key}=${encodeURIComponent(request.HeaderParams()[key])}`);
+            Object.keys(request.HeaderParams()).forEach( callback );
+         }
 
+         // fetch by url
+         const _path = searchParams ? `${request.URLPath}?${searchParams.join("&")}` : request.URLPath()
+         const url = new URL(_path, API_ORIGIN)
+         console.log(`OGDPopulationAPI is making a request to ${url}`)
+         let options = {
+            method : "POST"
+         };
+         if (Object.keys(request.BodyParams()).length > 0) {
+            options.body = JSON.stringify(request.BodyParams())
+         }
+         return fetch(url, options);
    }
    /**
     * @param {APIRequest} request 
     * @returns {Promise<Response>}
     */
    static fetchPUT(request) {
-
-   }
-
-   /**
-    * @param {APIRequest} request 
-    * @returns {Promise<Response>}
-    */
-   static fetchMetrics(request) {
-      if (request instanceof PopulationAPIRequest) {
-         if (request.start_date === null) {
-            console.warn("selection_options.start_date was null! Defaulting to today.")
+         let searchParams = null;
+         if (Object.keys(request.HeaderParams()).length > 0) {
+            searchParams = []
+            let callback = (key) => searchParams.push(`${key}=${encodeURIComponent(request.HeaderParams()[key])}`);
+            Object.keys(request.HeaderParams()).forEach( callback );
          }
-         if (request.end_date === null) {
-            console.warn("selection_options.end_date was null! Defaulting to today.")
-         }
-         const urlPath = `populations/metrics`;
-         let start_date = request.start_date ?? new Date();
-         let end_date   = request.end_date   ?? new Date();
-         const searchParams = new URLSearchParams({
-            game_id        : encodeURIComponent(request.Game),
-            start_datetime : encodeURIComponent(start_date.toISOString().split('T')[0]) + 'T00:00',
-            end_datetime   : encodeURIComponent(end_date.toISOString().split('T')[0]) + 'T23:59',
-            metrics        : encodeURIComponent(`[${request.Extractors}]`)
-         });
 
          // fetch by url
-         const url = new URL(`${urlPath}?${searchParams.toString()}`, API_ORIGIN)
+         const _path = searchParams ? `${request.URLPath}?${searchParams.join("&")}` : request.URLPath()
+         const url = new URL(_path, API_ORIGIN)
          console.log(`OGDPopulationAPI is making a request to ${url}`)
          let options = {
-            method : "POST"
+            method : "PUT"
          };
+         if (Object.keys(request.BodyParams()).length > 0) {
+            options.body = JSON.stringify(request.BodyParams())
+         }
          return fetch(url, options);
-      }
-      else {
-         throw new TypeError("Sent wrong type of selection options to Population API!");
-      }
    }
 }
