fce96344f38337dd94aae299613e77b92f9f9ba5
Luke Swanson <lwswanson2@wisc.edu>
2024-01-30 14:29:09 -0600

    Make sure all possible points of failure have a catch(...)
    This lets us make sure we return a real thing and don't have hanging
    errors.
    Users of the API should have case(s) to handle non-200 responses.

--- src/apis/OGDAPI.js
+++ src/apis/OGDAPI.js
@@ -22,6 +22,21 @@ export class OGDAPI {
       );
    } 
    /**
+    * Getter for a default response when the cached API response did not have valid JSON data.
+    * @returns {Response}
+    */
+   static get BadCache() {
+      return new Response(
+         JSON.stringify(
+            {
+               result: "Locally-cached data did not contain valid JSON.",
+               status: "FAILURE",
+               val: "{}"
+         }),
+         {status:500}
+      );
+   }
+   /**
     * Getter for a default response when the API interface had an error sending the request to the server.
     * @returns {Response}
     */
@@ -37,14 +52,14 @@ export class OGDAPI {
       );
    }
    /**
-    * Getter for a default response when the API interface had an error sending the request to the server.
+    * Getter for a default response when the API gave a response containing invalid JSON data.
     * @returns {Response}
     */
-   static get ErroredRequest() {
+   static get BadResponse() {
       return new Response(
          JSON.stringify(
             {
-               result: "Error converting API server response to APIResponse class.",
+               result: "API response did not contain valid JSON data",
                status: "FAILURE",
                val: "{}"
          }),
@@ -55,11 +70,11 @@ export class OGDAPI {
     * Getter for a default response when the API interface had an error sending the request to the server.
     * @returns {Response}
     */
-   static get BadCache() {
+   static get BadConversion() {
       return new Response(
          JSON.stringify(
             {
-               result: "Locally-cached data was not valid JSON.",
+               result: "Error converting API server response to APIResponse class.",
                status: "FAILURE",
                val: "{}"
          }),
@@ -72,20 +87,45 @@ export class OGDAPI {
     * @returns {Promise<APIResult>}
     */
    static fetch(request) {
+      /*
+         Overall how it works:
+         First, we need to check if data is cached or not.
+         If it isn't, we need to get data from server, cache it, and still pass that data on to return an APIResult.
+         Because this is dealing with fetch API, we need to return a promise in order to avoid having to sort out await-async madness.
+         Thus, if data was cached, we're still going to need to return a promise.
+         Hence, in either case we generate a Response object, and then get the .json() to return.
+
+         In uncached case, we have a .then(...) that uses the JSON to stick in the cache, and returns it.
+         In the already-cached case, we just take the JSON string and use it to init the Response object, and still return the .json() result.
+
+         Doing it this way, we always send back the JSON our APIResponse wants to init from,
+         but don't need to (a) do async stuff, nor (b) read json() from API's fetch Response more than once.
+
+         It's all pretty gross, but as straightforward as I can make it with JS's weird halfway, half-baked solutions to aynchronicity.
+      */
       /** @type {Promise<any>} */
       let ret_val = OGDAPI.NoRequest.json();
 
       if (request != null) {
          const localData = localStorage.getItem(request.LocalStorageKey);
-   // 1. If requested data is already cached, retrieve from cache
+   // 1. If requested data is not already cached, retrieve from server and store in cache
          if (!localData) {
             const url = request.FetchURL(API_ORIGIN, API_PATH);
             const options = request.FetchOptions();
             console.log(`OGDAPI is making a request to ${url}`)
             console.log(`Fetching into ${request.LocalStorageKey}`);
             ret_val = fetch(url, options)
+                     .catch(
+                        (error) => {
+                           console.warn(`Could not make API Request, got the following error:\n${error}`)
+                           return OGDAPI.FailedRequest.json();
+                     })
                      .then((response) => response.json())
-                     .catch((error) => console.error(`Could not parse JSON for response: ${error}`))
+                     .catch(
+                        (error) => {
+                           console.error(`Could not parse JSON from API response, got the following error:\n${error}`)
+                           return OGDAPI.BadResponse.json();
+                        })
                      .then((response_data) => {
                         localStorage.setItem(
                            request.LocalStorageKey,
@@ -93,28 +133,29 @@ export class OGDAPI {
                         );
                         return response_data;
                      })
-                     .catch(
-                        (error) => {
-                           console.warn(`Could not make API Request, got the following error:\n${error}`)
-                           return OGDAPI.FailedRequest.json();
-                     });
          }
          else {
-   // 2. If in cache, retrieve it to return.
-            // console.log(localData)
-            console.log(`Found ${request.LocalStorageKey} in the cache`);
-            try {
-               const parsed_data = JSON.parse(localData)
-               ret_val = new Response(parsed_data, {status:200}).json()
-            } catch (err) {
-               console.error(`Local data (${localData}) was not valid JSON!\nResulted in error ${err}`);
-               ret_val = OGDAPI.BadCache.json();
-            }
+   // 2. If data is in cache, place it into a Response and return the json.
+            console.log(`Found ${request.LocalStorageKey} in the cache.`);
+            // console.log(`Contents of cached data were: ${localData}`)
+            ret_val = new Response(localData, {status:200})
+                     .json()
+                     .catch(
+                        (error) => {
+                           console.log(`Could not get JSON from cached response object, an error occurred!\n${error}`);
+                           return OGDAPI.BadCache.json()
+                     })
          }
       }
-   // 3. Whether we got from fetch or cache, we set up as response object
+      // else { ret_val = OGDAPI.NoRequest.json() }, which is the default for ret_val
+   // 3. Whether we got from fetch or cache, we take the JSON from the response and use it to populate an APIResult.
       return ret_val
             .then((response) => new APIResult(response))
+            .catch(
+               (error) => {
+                           console.log(`Could not convert Response JSON to an APIResult object, an error occurred!\n${error}`);
+                           return OGDAPI.BadConversion.json()
+            })
    }
 
 }
