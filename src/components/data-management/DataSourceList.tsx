a8f9234bc640bbabbee25cd35308e4acc3bfcb80
TheAlmightyHelix <ccyyhh97@hotmail.com>
2025-07-03 15:31:39 -0500

    Add split dataset button to DataSourceList for improved dataset management

--- src/components/data-management/DataSourceList.tsx
+++ src/components/data-management/DataSourceList.tsx
@@ -1,6 +1,6 @@
 import useDataStore from '../../store/useDataStore';
 import FilePicker from './FilePicker';
-import { X } from 'lucide-react';
+import { ScissorsLineDashed, X } from 'lucide-react';
 
 const DataSourceList = () => {
   const { datasets, removeDataset } = useDataStore();
@@ -27,6 +27,13 @@ const DataSourceList = () => {
                     {dataset.featureLevel}
                   </div>
                 </div>
+                <button
+                  onClick={() => removeDataset(dataset.id)}
+                  className="ml-2 p-1 text-gray-400 hover:text-blue-500 transition-colors"
+                  title="Split dataset"
+                >
+                  <ScissorsLineDashed className="w-4 h-4" />
+                </button>
                 <button
                   onClick={() => removeDataset(dataset.id)}
                   className="ml-2 p-1 text-gray-400 hover:text-red-500 transition-colors"
